5b8bc65d0306bc51a82f1394a10ad26ba9614c9f
azixkhan <azixkhan.55@gmail.com>
2023-10-02 23:25:32 +0500

    fingerprint & face lock added

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:mytasks/Screens/welcome_screen.dart';
 import 'package:mytasks/Utils/custom_container.dart';
 import 'package:mytasks/Utils/custom_drawer.dart';
 import 'package:mytasks/provider/theme_changer_provider.dart';
 import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
-  List<String> images = [
-    "https://static.javatpoint.com/tutorial/flutter/images/flutter-logo.png",
-    "https://static.javatpoint.com/tutorial/flutter/images/flutter-logo.png",
-    "https://static.javatpoint.com/tutorial/flutter/images/flutter-logo.png",
-    "https://static.javatpoint.com/tutorial/flutter/images/flutter-logo.png"
-  ];
-  HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late final LocalAuthentication auth;
+  bool _supportState = false;
+
+  @override
+  void initState() {
+    super.initState();
+    auth = LocalAuthentication();
+    _checkBiometricSupport();
+  }
+
+  Future<void> _checkBiometricSupport() async {
+    bool isSupported = await auth.isDeviceSupported();
+    if (mounted) {
+      setState(() {
+        _supportState = isSupported;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final themeChanger = Provider.of<ThemeChangerProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         foregroundColor: Colors.white,
@@ -56,24 +77,46 @@ class HomeScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Expanded(
-                child: Container(
-                    padding: EdgeInsets.all(12.0),
-                    child: GridView.builder(
-                      itemCount: images.length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          crossAxisSpacing: 4.0,
-                          mainAxisSpacing: 4.0),
-                      itemBuilder: (BuildContext context, int index) {
-                        return Image.network(images[index]);
-                      },
-                    )),
+              if (_supportState)
+                const Text('This device is supported')
+              else
+                const Text('This device is not supported'),
+              const Divider(
+                height: 100,
               ),
+              ElevatedButton(
+                  onPressed: _getAvailableBiometrics,
+                  child: Text('Get Available biometrics')),
+              const Divider(
+                height: 100,
+              ),
+              ElevatedButton(
+                  onPressed: _authenticate, child: Text('Authenticate')),
             ],
-          )
+          ),
         ],
       ),
     );
+  }
+
+  Future<void> _authenticate() async {
+    try {
+      bool authenticated = await auth.authenticate(
+        localizedReason: 'Authenticate to continue',
+        options: const AuthenticationOptions(
+          biometricOnly: false,
+          stickyAuth: true,
+        ),
+      );
+      print('Authenticated: $authenticated');
+    } on PlatformException catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _getAvailableBiometrics() async {
+    List<BiometricType> availableBiometrics =
+        await auth.getAvailableBiometrics();
+    print('List of available biometrics $availableBiometrics');
   }
 }
